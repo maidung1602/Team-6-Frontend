257c44a3c1bb33c5a85f0894414d01fa0f080982
Lum <5chhoang5@gmail.com>
2023-03-03 19:05:23 +0700

    matching 3

---
+++ matching-remake/matching-remake.js
@@ -0,0 +1,104 @@
+const pokeAPIBaseUrl = "https://pokeapi.co/api/v2/pokemon/";
+const game = document.getElementById('game');
+
+let firstPick;
+let isPaused = true;
+let matches;
+
+const loadCard = async () => {
+    const randomIds = new Set();
+    while(randomIds.size < 8){
+        const randomNumber = Math.ceil(Math.random() * 150);
+        randomIds.add(randomNumber);
+    }
+    const cardPromises = [...randomIds].map(id => fetch(pokeAPIBaseUrl + id))
+    const results = await Promise.all(cardPromises);
+    return await Promise.all(results.map(res => res.json()));
+}
+
+const joinGame = async() => {
+    game.innerHTML = '';
+    isPaused = true;
+    firstPick = null;
+    matches = 0;
+    setTimeout(async () => {
+        const loadedCard = await loadCard();
+        displayCard([...loadedCard, ...loadedCard]);
+        isPaused = false;
+    },200)
+}
+
+const displayCard = (card) => {
+    card.sort(_ => Math.random() - 0.5);
+    const cardHTML = card.map(card => {
+        return `
+          <div class="card" onclick="clickCard(event)" data-pokename="${card.name}">
+            <div class="deco not_chosen">
+            <img src="${card.sprites.front_default}" alt="${card.name}"  />
+            <h2>${card.name}</h2>
+            </div>
+        </div>
+    `}).join('');
+    game.innerHTML = cardHTML;
+}
+
+const clickCard = (e) => {
+    const wordCard = e.currentTarget;
+    if(wordCard.classList.contains('corrected') || isPaused) {
+        return;
+    }
+    if(wordCard == firstPick) {
+        firstPick.querySelector('.deco').classList.add('not_chosen');
+        firstPick.id = '';
+        firstPick = null;
+        isPaused = false;
+        return
+    }
+    isPaused = true;
+    if(!firstPick){
+        firstPick = wordCard;
+        firstPick.id = 'chosen'
+        firstPick.querySelector('.not_chosen').classList.remove('not_chosen');
+        isPaused = false;
+    }
+    else {
+        wordCard.querySelector('.not_chosen').classList.remove('not_chosen');
+        const secondCardName = wordCard.dataset.pokename;
+        const firstCardName = firstPick.dataset.pokename;
+        if(firstCardName !== secondCardName) {
+            callWrongAnimation(wordCard, firstPick);
+            firstPick.id = '';
+            firstPick = null;
+        }else {
+            wordCard.classList.add('corrected');
+            firstPick.classList.add('corrected');
+            firstPick.id = '';
+            matches++;
+            if(matches === 8) {
+                console.log("WINNER");
+            }
+            firstPick = null;
+            isPaused = false;
+        }
+    }
+    
+}
+
+function callWrongAnimation(c1, c2) {
+    c1.classList.add('wrong');
+    c2.classList.add('wrong');
+    setTimeout(() => {
+        c1.classList.remove('wrong');
+        c2.classList.remove('wrong');
+        c1.querySelector('.deco').classList.add('not_chosen');
+        c2.querySelector('.deco').classList.add('not_chosen');
+        isPaused = false;
+    }, 1000);
+};
+
+// const rotateElements = (elements) => {
+//     if(typeof elements !== 'object' || !elements.length) return;
+//     elements.forEach(element => element.classList.toggle('rotated'));
+// }
+
+joinGame();
